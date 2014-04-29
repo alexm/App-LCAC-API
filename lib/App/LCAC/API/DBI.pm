0d26842e6b953cf58ab1c7d7679c2ab6a0395fde
Alex Muntada <alexm@alexm.org>
2014-04-29 19:14:50 +0200

    refactor db schemas out of code

--- lib/App/LCAC/API/DBI.pm
+++ lib/App/LCAC/API/DBI.pm
@@ -5,10 +5,12 @@ package App::LCAC::API::DBI;
 
 =head1 SYNOPSIS
 
+    use YAML 'LoadFile';
+    my $config = LoadFile('db.yml');
     use App::LCAC::API::DBI;
-    my $dbh = App::LCAC::API::DBI->categorizer($db_dir);
-    $dbh->prepare('SELECT * FROM site.categ');
-    $dbh->execute();
+    my $dbh = App::LCAC::API::DBI->connect( db => $config->{dbname1} );
+    my $query = $dbh->prepare('SELECT * FROM tablename1');
+    $query->execute();
 
 =head1 DESCRIPTION
 
@@ -20,135 +22,70 @@ perform SQL queries on them.
 use DBI;
 use File::Spec::Functions qw( catfile );
 
-sub __connect {
+=method connect( %params )
+
+Returns a new DBI handler to a CSV database.
+
+=head2 Params:
+
+=over 4
+
+=item * path
+
+Path where the CSV tables should be found, defaults to current directory.
+
+=item * db
+
+Hash ref with CSV schema and configuration values for L<DBD::CSV>.
+
+Examples of schemas in YAML:
+
+    'dbname1':
+      csv_sep_char: ':'
+      csv_allow_whitespace: 1
+      csv_allow_loose_quotes: 1
+      csv_tables:
+        'tablename1':
+          col_names:
+            - 'colname1'
+            - 'colname2'
+    'db2':
+      csv_sep_char: ';'
+      csv_tables:
+        't1':
+          col_names:
+            - 'c1'
+            - 'c2'
+
+=cut
+
+sub connect {
+    my $class = shift;
     my %params = @_;
+    $params{path} ||= '.';
 
     my $dbh = DBI->connect('dbi:CSV:')
         or die "Cannot connect: $DBI::errstr\n";
 
-    die "Hash ref tables is required as a param"
-        unless ref $params{tables} eq 'HASH';
+    die "Hash ref db is required as a param"
+        unless ref $params{db} eq 'HASH';
 
-    $dbh->{csv_sep_char}           = $params{sep_char}           || ':';
-    $dbh->{csv_allow_whitespace}   = $params{allow_whitespace}   || 1;
-    $dbh->{csv_allow_loose_quotes} = $params{allow_loose_quotes} || 1;
-
-    # DBI csv_tables is an interface that looks like a hash
-    # but it does not work always as a hash, i.e. key values
-    # must be copied one by one in order to work as expected.
-    while ( (my $key, my $value) = each %{ $params{tables} } ) {
-        $dbh->{csv_tables}{$key} = $value;
+    for my $p (keys %{ $params{db} }) {
+        if ($p eq 'csv_tables') {
+            # DBI csv_tables is an interface that looks like a hash
+            # but it does not work always as a hash, i.e. key values
+            # must be copied one by one in order to work as expected.
+            while ( (my $key, my $value) = each %{ $params{db}{csv_tables} } ) {
+                $dbh->{csv_tables}{$key} = $value;
+                $dbh->{csv_tables}{$key}{file} = catfile( $params{path}, $key );
+            }
+        }
+        else {
+            $dbh->{$p} = $params{db}{$p};
+        }
     }
 
     return $dbh;
-}
-
-=method categorizer( $db_dir )
-
-Returns a new DBI handler to LCAC Categorizer database.
-
-=cut
-
-sub categorizer {
-    my ( $class, $db_dir ) = @_;
-
-    my %tables = (
-        'site.categ' => {
-            file      => catfile( $db_dir, 'site.categ' ),
-            col_names => [qw( category members )],
-        },
-    );
-
-    return __connect( tables => \%tables );
-}
-
-=method netdb( $db_dir )
-
-Returns a new DBI handler to LCAC NetDB database.
-
-=cut
-
-sub netdb {
-    my ( $class, $db_dir ) = @_;
-
-    my %tables = (
-        'Equipment'            => { col_names => [qw(
-            Name
-            Type
-            Brand
-            Model
-            Monitor
-            KeyboardType
-            SerialNumber
-            Description
-            Comments
-        )]},
-        'Equipment.Admin'      => { col_names => [qw(
-            Name
-            EquipmentGroup
-            Owner
-            Admin
-            UPCInventario
-            BuyDate
-            FechaFinGarantia
-            FechaAlta
-            NumeroPedido
-        )]},
-        'EquipmentGroup'       => { col_names => [qw(
-            Name
-            IPPool
-        )]},
-        'IPAddress'            => { col_names => [qw(
-            IP
-            Name
-        )]},
-        'IPName.Service'       => { col_names => [qw(
-            Name
-            Services
-        )]},
-        'IPPool'               => { col_names => [qw(
-            Name
-            IPList
-        )]},
-        'MAC-IP'               => { col_names => [qw(
-            MAC
-            IPorIPPool
-        )]},
-        'NAP'                  => { col_names => [qw(
-            Building
-            Name
-            Room
-            Workspace
-            State
-            Map
-            MapLocation
-            Tags
-            Comments
-        )]},
-        'NAP-NIP'              => { col_names => [qw(
-            Building
-            Name
-            EquipmentName
-            EthName
-        )]},
-        'NIP'                  => { col_names => [qw(
-            EquipmentName
-            EthName
-            SpeedMbps
-            MAC
-        )]},
-        'VLAN'                 => { col_names => [qw(
-            Number
-            Name
-            IPPool
-        )]},
-    );
-
-    while ( (my $key, my $value) = each %tables ) {
-        $tables{$key}{file} = catfile( $db_dir, $key );
-    }
-
-    return __connect( tables => \%tables );
 }
 
 1;
